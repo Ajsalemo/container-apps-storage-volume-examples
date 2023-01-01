dcd4c4d2d3fe1cdfbf1f473a08fdd04e68d7108a
Anthony Salemo <ajssalemo@gmail.com>
2023-01-01 18:02:09 -0500

    added a read file controller

--- container-storage/node/server.js
+++ container-storage/node/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { homeController } from "./controllers/homeController.js";
 import { writeFileController } from "./controllers/writeFileController.js";
+import { readFileController } from "./controllers/readFileController.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use("/", homeController);
 app.use("/api/write", writeFileController);
+app.use("/api/read", readFileController);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}.`));

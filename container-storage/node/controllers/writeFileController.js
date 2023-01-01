51074f755175ef383162881caa54cab38d9b6d2d
Anthony Salemo <ajssalemo@gmail.com>
2023-01-01 17:07:54 -0500

    able to write a file locally with node

--- container-storage/node/controllers/writeFileController.js
+++ container-storage/node/controllers/writeFileController.js
@@ -1,12 +1,18 @@
 import * as express from "express";
-import { writeFile } from "fs"
+import { v4 as uuidv4 } from "uuid";
+import { writeFile } from "fs";
+import { resolve } from "path";
 
-
-const router = express.Router()
+const router = express.Router();
 
 export const writeFileController = router.get("/", (_req, res) => {
-    const content = "This is a file being stored on container storage"
+  const content = "This is a file being stored on container storage";
+  const fileName = `${uuidv4()}.txt`;
+  const path = resolve("files")
 
-    writeFile()
-    res.json({ msg: "container-storage-node" })
-})
+  writeFile(`${path}/${fileName}`, content, (err) => {
+    if (err) return res.status(500).json({ error: err });
+
+    res.json({ msg: `File name ${fileName} created.` });
+  });
+});
--- container-storage/node/server.js
+++ container-storage/node/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { homeController } from "./controllers/homeController.js";
+import { writeFileController } from "./controllers/writeFileController.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use("/", homeController);
+app.use("/api/write", writeFileController);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}.`));
